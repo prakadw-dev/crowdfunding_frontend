596162aa6885d5fb7e1badb85f9d0d7a17749805
prakadw-dev <prakasitadewi@gmail.com>
2026-04-05 12:56:53 +1000

    added some CSS

--- src/components/FundraiserCard.jsx
+++ src/components/FundraiserCard.jsx
@@ -8,7 +8,7 @@ function FundraiserCard(props) {
   return (
     <div className="fundraiser-card">
       <Link to={fundraiserLink}>
-        {/* <img src={fundraiserData.image} alt={fundraiserData.title} /> */}
+        <img src={fundraiserData.image} alt={fundraiserData.title} />
         <h3>{fundraiserData.title}</h3>
       </Link>
     </div>
